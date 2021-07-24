5adc623091d14b45f19cf537e736eb7f03ebac57
shluv1217 <hkluv1009@gmail.com>
2021-07-24 09:27:15 +0900

    Add mailindex Controller
    Change-Id: Icc99bc5a6af870bfd79e63601fab8f09e8659167

---
+++ test/src/main/java/com/example/test/controller/MailIndexC.java
@@ -0,0 +1,18 @@
+package com.example.test.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/mail/")
+public class MailIndexC {
+	
+	Logger logger = LoggerFactory.getLogger(MailIndexC.class);
+
+	
+
+}
